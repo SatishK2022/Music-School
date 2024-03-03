3b676c3a1d0b6117eaf73496a538930ecd9e7b1b
SatishK2022 <sk7399052@gmail.com>
2024-03-03 12:28:12 +0530

    Updated contact and courses page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { BackgroundBeams } from "@/components/ui/background-beams";
 import { FormEvent, useState } from "react";
 
@@ -12,39 +13,6 @@ const page = () => {
   };
 
   return (
-    // <div className="min-h-screen relative max-w-5xl flex flex-col items-center mx-auto bg-black py-12 pt-32">
-    //   <h1 className="text-lg md:text-5xl text-center font-sans font-bold mb-8 text-white">
-    //     Contact Us
-    //   </h1>
-    //   <p className="text-gray-400 w-1/2 text-center">
-    //     We are here to help with any questions about our courses, programs, or
-    //     events. Reach out and let us know how we can assist you in your musical
-    //     journey.
-    //   </p>
-    //   <form className="w-1/2 flex flex-col items-center gap-5 py-10">
-    //     <input
-    //       type="text"
-    //       required
-    //       placeholder="Your email address"
-    //       className="w-full px-5 py-2 rounded bg-transparent border border-gray-500"
-    //     />
-    //     <textarea
-    //       rows={5}
-    //       required
-    //       placeholder="Your message"
-    //       className="w-full px-5 py-2 rounded bg-transparent border border-gray-500"
-    //     />
-    //     <button
-    //       type="submit"
-    //       className="bg-white w-full text-black px-4 py-2 rounded-md mt-4"
-    //     >
-    //       Send
-    //     </button>
-    //   </form>
-
-    //   <Meteors number={20} />
-    // </div>
-
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 py-12 pt-36 relative">
       <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
       <div className="max-w-2xl mx-auto p-4 relative z-10">
